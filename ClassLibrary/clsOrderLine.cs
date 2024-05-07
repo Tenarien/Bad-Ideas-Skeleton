b6d70568ce012d787e40c6c5f8cb34a091369906
p2755807 <p2755807@my365.dmu.ac.uk>
2024-05-07 14:49:12 +0100

    Add more test and Create rest of the properties

--- ClassLibrary/clsOrderLine.cs
+++ ClassLibrary/clsOrderLine.cs
@@ -2,5 +2,10 @@
 {
     public class clsOrderLine
     {
+        public int OrderLineId { get; set; }
+        public int OrderId { get; set; }
+        public int BookId { get; set; }
+        public int Quantity { get; set; }
+        public bool OrderLineStatus { get; set; }
     }
 }
--- Testing6/tstOderLine.cs
+++ Testing6/tstOderLine.cs
@@ -14,5 +14,65 @@ namespace Testing6
 
             Assert.IsNotNull(AnOrderLine);
         }
+
+        [TestMethod]
+        public void OrderLineIdPropertyOK() 
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+
+            int TestData = 1;
+
+            AnOrderLine.OrderLineId = TestData;
+
+            Assert.AreEqual(AnOrderLine.OrderLineId, TestData);
+        }
+
+        [TestMethod]
+        public void OrderIdPropertyOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+
+            int TestData = 1;
+
+            AnOrderLine.OrderId = TestData;
+
+            Assert.AreEqual(AnOrderLine.OrderId, TestData);
+        }
+
+        [TestMethod]
+        public void BookIdPropertyOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+
+            int TestData = 1;
+
+            AnOrderLine.BookId = TestData;
+
+            Assert.AreEqual(AnOrderLine.BookId, TestData);
+        }
+
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+
+            int TestData = 1;
+
+            AnOrderLine.Quantity = TestData;
+
+            Assert.AreEqual(AnOrderLine.Quantity, TestData);
+        }
+
+        [TestMethod]
+        public void OrderLineStatusPropertyOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+
+            Boolean TestData = true;
+
+            AnOrderLine.OrderLineStatus = TestData;
+
+            Assert.AreEqual(AnOrderLine.OrderLineStatus, TestData);
+        }
     }
 }
