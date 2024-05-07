e675097427c4f188b3bc686c65b1afc7607b543c
Rania Hassan <P2763471@my365.dmu.ac.uk>
2024-05-07 13:16:41 +0100

    Created test for all properties

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,7 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
-        public clsOrder() { } 
+        public clsOrder() { }
+
+        public bool ShippingStatus { get; set; }
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string ShippingAddress { get; set; }
     }
 }
--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,61 @@ namespace Testing2
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void ShippingStatusPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.ShippingStatus = TestData;
+            Assert.AreEqual(AnOrder.ShippingStatus, TestData);
+        }
+
+        [TestMethod]
+        public void OrderIdPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.OrderId = TestData;
+            Assert.AreEqual(AnOrder.OrderId, TestData);
+        }
+        
+        [TestMethod]
+        public void UserIdPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.UserId = TestData;
+            Assert.AreEqual(AnOrder.UserId, TestData);
+        }
+
+        [TestMethod]
+        public void OrderDatePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.OrderDate = TestData;
+            Assert.AreEqual(AnOrder.OrderDate, TestData);
+        }
+        
+        [TestMethod]
+        public void TotalPricePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Decimal TestData = 6;
+            AnOrder.TotalPrice = TestData;
+            Assert.AreEqual(AnOrder.TotalPrice, TestData);
+        }
+        
+        [TestMethod]
+        public void ShippingAddressPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string TestData = "123 Maple Street London W1A 1AA United Kingdom";
+            AnOrder.ShippingAddress = TestData;
+            Assert.AreEqual(AnOrder.ShippingAddress, TestData);
+        }
+
 
     }
 }
