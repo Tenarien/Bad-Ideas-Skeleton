e26750e8a19f15ca21cc895a917ec52f5c403915
p2755807 <p2755807@my365.dmu.ac.uk>
2024-05-09 09:55:32 +0100

    Create find test methods and Change the class properties to private

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -3,16 +3,121 @@
 namespace ClassLibrary
 {
     public class clsStaff
-    {
-        public clsStaff()
+    {   
+        //private data property for the class staff
+        private Int32 mStaffId;
+        private String mStaffName;
+        private String mStaffAddress;
+        private DateTime mStaffDate;
+        private String mStaffRole;
+        private Boolean mStaffPrivilage;
+
+        
+
+        //public property for staff id
+        public Int32 StaffId
         {
+            //sends data out of property
+            get 
+            { 
+                return mStaffId;
+            }
+            //allows data into the property
+            set 
+            { 
+                mStaffId = value;
+            }
         }
 
-        public int StaffId { get; set; }
-        public string StaffName { get; set; }
-        public string StaffAddress { get; set; }
-        public DateTime StaffDate { get; set; }
-        public string StaffRole { get; set; }
-        public bool StaffPrivilage { get; set; }
+        //public property for staff name
+        public String StaffName
+        {
+            //sends data out of property
+            get
+            {
+                return mStaffName;
+            }
+            //allows data into the property
+            set
+            {
+                mStaffName = value;
+            }
+        }
+
+        //public property for staff address
+        public String StaffAddress
+        {
+            //sends data out of property
+            get
+            {
+                return mStaffAddress;
+            }
+            //allows data into the property
+            set
+            {
+                mStaffAddress = value;
+            }
+        }
+
+        //public property for staff date added
+        public DateTime StaffDate
+        {
+            //sends data out of property
+            get
+            {
+                return mStaffDate;
+            }
+            //allows data into the property
+            set
+            {
+                mStaffDate = value;
+            }
+        }
+
+        //public property for staff role
+        public String StaffRole
+        {
+            //sends data out of property
+            get
+            {
+                return mStaffRole;
+            }
+            //allows data into the property
+            set
+            {
+                mStaffRole = value;
+            }
+        }
+
+        //public property for staff address
+        public Boolean StaffPrivilage
+        {
+            //sends data out of property
+            get
+            {
+                return mStaffPrivilage;
+            }
+            //allows data into the property
+            set
+            {
+                mStaffPrivilage = value;
+            }
+        }
+
+
+
+        public bool Find(int StaffId)
+        {
+            //set private data to test data value
+            mStaffId = 1;
+            mStaffName = "Joe";
+            mStaffAddress = "12 Test Street";
+            mStaffDate = Convert.ToDateTime("23/12/2022");
+            mStaffRole = "Staff";
+            mStaffPrivilage = true;
+
+            //always return true
+            return true;
+        }
     }
 }
--- Testing1/tstStaff.cs
+++ Testing1/tstStaff.cs
@@ -10,8 +10,9 @@ namespace Testing1
         [TestMethod]
         public void InstanceOK()
         {
+            //create an instance of the class
             clsStaff aStaff = new clsStaff();
-
+            //test to see if the result is true
             Assert.IsNotNull(aStaff);
         }
 
@@ -19,61 +20,214 @@ namespace Testing1
         [TestMethod]
         public void StaffIdPropertyOK()
         {
+            //create an instance of the class
             clsStaff aStaff = new clsStaff();
+            //declare a variable and assign it to the property
             int TestData = 1;
             aStaff.StaffId = TestData;
-
+            //test to see if the result is true
             Assert.AreEqual(aStaff.StaffId, TestData);
         }
 
         [TestMethod]
         public void StaffNamePropertyOK()
         {
+            //create an instance of the class
             clsStaff aStaff = new clsStaff();
+            //declare a variable and assign it to the property
             string TestData = "John";
             aStaff.StaffName = TestData;
-
+            //test to see if the result is true
             Assert.AreEqual(aStaff.StaffName, TestData);
         }
 
         [TestMethod]
         public void StaffAddressPropertyOK()
         {
+            //create an instance of the class
             clsStaff aStaff = new clsStaff();
+            //declare a variable and assign it to the property
             string TestData = "13 London Road";
             aStaff.StaffAddress = TestData;
-
+            //test to see if the result is true
             Assert.AreEqual(aStaff.StaffAddress, TestData);
         }
 
         [TestMethod]
         public void StaffDatePropertyOK()
         {
+            //create an instance of the class
             clsStaff aStaff = new clsStaff();
+            //declare a variable and assign it to the property
             DateTime TestData = DateTime.Now.Date;
             aStaff.StaffDate = TestData;
-
+            //test to see if the result is true
             Assert.AreEqual(aStaff.StaffDate, TestData);
         }
 
         [TestMethod]
         public void StaffRolePropertyOK()
         {
+            //create an instance of the class
             clsStaff aStaff = new clsStaff();
+            //declare a variable and assign it to the property
             string TestData = "Customer Help";
             aStaff.StaffRole = TestData;
-
+            //test to see if the result is true
             Assert.AreEqual(aStaff.StaffRole, TestData);
         }
 
         [TestMethod]
         public void StaffPrivilagePropertyOK()
         {
+            //create an instance of the class
             clsStaff aStaff = new clsStaff();
-            bool TestData = true;
+            //declare a variable and assign it to the property
+            Boolean TestData = true;
             aStaff.StaffPrivilage = TestData;
-
+            //test to see if the result is true
             Assert.AreEqual(aStaff.StaffPrivilage, TestData);
         }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class
+            clsStaff aStaff = new clsStaff();
+            //declare a variable and assign it to the property
+            Boolean Found = false;
+            Int32 StaffId = 1;
+            //invoke the method
+            Found = aStaff.Find(StaffId);
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestStaffIdFound()
+        {
+            //create an instance of the class
+            clsStaff aStaff = new clsStaff();
+            //declare a variable
+            Boolean Found = false;
+            Boolean OK = true;
+
+            //declare a variable and assign it to the property
+            Int32 StaffId = 1;
+            //invoke the method
+            Found = aStaff.Find(StaffId);
+            //test to see if the result is true
+            if (aStaff.StaffId != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffNameFound()
+        {
+            //create an instance of the class
+            clsStaff aStaff = new clsStaff();
+            //declare a variable
+            Boolean Found = false;
+            Boolean OK = true;
+
+            //declare a variable and assign it to the property
+            Int32 StaffId = 1;
+            //invoke the method
+            Found = aStaff.Find(StaffId);
+            //test to see if the result is true
+            if (aStaff.StaffName != "Joe")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffAddressFound()
+        {
+            //create an instance of the class
+            clsStaff aStaff = new clsStaff();
+            //declare a variable
+            Boolean Found = false;
+            Boolean OK = true;
+
+            //declare a variable and assign it to the property
+            Int32 StaffId = 1;
+            //invoke the method
+            Found = aStaff.Find(StaffId);
+            //test to see if the result is true
+            if (aStaff.StaffAddress != "12 Test Street")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffDateFound()
+        {
+            //create an instance of the class
+            clsStaff aStaff = new clsStaff();
+            //declare a variable
+            Boolean Found = false;
+            Boolean OK = true;
+
+            //declare a variable and assign it to the property
+            Int32 StaffId = 1;
+            //invoke the method
+            Found = aStaff.Find(StaffId);
+            //test to see if the result is true
+            if (aStaff.StaffDate != Convert.ToDateTime("23/12/2022"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffRoleFound()
+        {
+            //create an instance of the class
+            clsStaff aStaff = new clsStaff();
+            //declare a variable
+            Boolean Found = false;
+            Boolean OK = true;
+
+            //declare a variable and assign it to the property
+            Int32 StaffId = 1;
+            //invoke the method
+            Found = aStaff.Find(StaffId);
+            //test to see if the result is true
+            if (aStaff.StaffRole != "Staff")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStaffPrivilageFound()
+        {
+            //create an instance of the class
+            clsStaff aStaff = new clsStaff();
+            //declare a variable
+            Boolean Found = false;
+            Boolean OK = true;
+
+            //declare a variable and assign it to the property
+            Int32 StaffId = 1;
+            //invoke the method
+            Found = aStaff.Find(StaffId);
+            //test to see if the result is true
+            if (aStaff.StaffPrivilage != true)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
     }
 }
