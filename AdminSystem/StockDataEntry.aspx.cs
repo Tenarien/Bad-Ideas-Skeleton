b7b7d5c2e118b19c8f32fca794ccdc1cbdec66d8
p2765164 <p2765164@my365.dmu.ac.uk>
2024-05-09 10:50:03 +0100

    added presentation layer for stock

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,11 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //Create a new instance of clsStock
+        clsStock aBook = new clsStock();
+        //Capture the title
+        aBook.Title = txtTitle.Text;
+        //Capture the book id
+        aBook.BookId = Convert.ToInt32(txtBookId.Text);
+        //Capture the date added
+        aBook.DateAdded = Convert.ToDateTime(DateTime.Now);
+        //Capture the price
+        aBook.Price = Convert.ToInt64(txtPrice.Text);
+        //Capture the quantity
+        aBook.Quantity = Convert.ToInt32(txtQuantity.Text);
+        //Capture the supplier id
+        aBook.SupplierId = Convert.ToInt32(txtSupplierId.Text);
+        //Capture the availability
+        aBook.Available = chkAvailable.Checked;
+        //Store the book in the session object
+        Session["aBook"] = aBook;
+        //Navigate to the view page
+        Response.Redirect("StockViewer.aspx");
     }
 }
--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clasStock
+        clsStock aBook = new clsStock();
+        //get the data from the session object
+        aBook = (clsStock)Session["aBook"];
+        //display the title for this entry
+        Response.Write(aBook.BookId);
+        Response.Write(aBook.Title);
+        Response.Write(aBook.DateAdded);
+        Response.Write(aBook.Price);
+        Response.Write(aBook.Quantity);
+        Response.Write(aBook.SupplierId);
+        Response.Write(aBook.Available);
     }
 }
