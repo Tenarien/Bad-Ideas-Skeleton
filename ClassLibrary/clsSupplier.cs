6102cdbbfff1ee41d0b1b951cfb1854cae05d3af
p2763705 <p2763705@GWHXX283WIN018W>
2024-05-15 12:38:10 +0100

    all errors emoved

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,9 +1,17 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
-        public clsSupplier()
-        {
-        }
+        public Int32 SupplierId { get; set; }
+
+        public string SupplierName { get; set;}
+
+        public DateTime ContractDate { get; set; }
+
+        public bool AvailableSupplier { get; set; }
+
+        public string SupplierAddress { get; set; }
     }
 }
