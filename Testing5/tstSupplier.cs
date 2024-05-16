c46a0279189276cee83d454af711b9b6285ce8d2
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-05-16 09:11:45 +0100

    Revert "ok"

--- Testing5/tstSupplier.cs
+++ Testing5/tstSupplier.cs
@@ -5,7 +5,7 @@ using System;
 namespace Testing5
 {
     [TestClass]
-    public class InstanceOK()
+    public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
@@ -14,21 +14,99 @@ namespace Testing5
             Assert.IsNotNull(AnSupplier);
         }
     }
+    [TestMethod]
+    public void ActivePropertyOK()
+    {
+        //create an instance of the class we want to create
+        clsAddress AnAddress = new clsAddress();
+        //create some test data to assign to the property
+        Boolean TestData = true;
+        //assign the data to the property
+        AnAddress.Active = TestData;
+        //test to see that the two values are the same
+        Assert.AreEqual(AnAddress.Active, TestData);
+    }
 
     [TestMethod]
-    public void SupplierId()
+    public void DateAddedPropertyOK()
     {
-
-        //create an instance of the class we want to increase
-        clsSupplier AnSupplier = new clsSupplier();
-        //create some test data to the property
-        String TestData = "12";
+        //create an instance of the class we want to create
+        clsAddress AnAddress = new clsAddress();
+        //create some test data to assign to the property
+        DateTime TestData = DateTime.Now.Date;
         //assign the data to the property
-        AnSupplier.Active = TestData;
-        //test to see the value
-        Assert.AreEqual(AnSupplier.Active,TestData);
+        AnAddress.DateAdded = TestData;
+        //test to see that the two values are the same
+        Assert.AreEqual(AnAddress.DateAdded, TestData);
     }
+
+    [TestMethod]
+    public void AddressIdPropertyOK()
+    {
+        //create an instance of the class we want to create
+        clsAddress AnAddress = new clsAddress();
+        //create some test data to assign to the property
+        Int32 TestData = 1;
+        //assign the data to the property
+        AnAddress.AddressId = TestData;
+        //test to see that the two values are the same
+        Assert.AreEqual(AnAddress.AddressId, TestData);
+    }
+
+
+
+    [TestMethod]
+    public void CountyCodePropertyOK()
+    {
+        //create an instance of the class we want to create
+        clsAddress AnAddress = new clsAddress();
+        //create some test data to assign to the property
+        Int32 TestData = 1;
+        //assign the data to the property
+        AnAddress.CountyCode = TestData;
+        //test to see that the two values are the same
+        Assert.AreEqual(AnAddress.CountyCode, TestData);
+    }
+
+    [TestMethod]
+    public void HouseNoPropertyOK()
+    {
+        //create an instance of the class we want to create
+        clsAddress AnAddress = new clsAddress();
+        //create some test data to assign to the property
+        string TestData = "21b";
+        //assign the data to the property
+        AnAddress.HouseNo = TestData;
+        //test to see that the two values are the same
+        Assert.AreEqual(AnAddress.HouseNo, TestData);
+    }
+
+    [TestMethod]
+    public void PostCodePropertyOK()
+    {
+        //create an instance of the class we want to create
+        clsAddress AnAddress = new clsAddress();
+        //create some test data to assign to the property
+        string TestData = "LE1 4AB";
+        //assign the data to the property
+        AnAddress.PostCode = TestData;
+        //test to see that the two values are the same
+        Assert.AreEqual(AnAddress.PostCode, TestData);
+    }
+
+    [TestMethod]
+    public void StreetPropertyOK()
+    {
+        //create an instance of the class we want to create
+        clsAddress AnAddress = new clsAddress();
+        //create some test data to assign to the property
+        string TestData = "Some Street";
+        //assign the data to the property
+        AnAddress.Street = TestData;
+        //test to see that the two values are the same
+        Assert.AreEqual(AnAddress.Street, TestData);
+    }
+
+
 }
-
-
 
