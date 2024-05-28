29044f7334fe66e46eb28065438612b09f5c5bc4
Syeda Tuli <P2757895@my365.dmu.ac.uk>
2024-05-28 18:01:34 +0100

    Created Properties for the Payment component

--- ClassLibrary/clsOrderLine.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ClassLibrary
-{
-    public class clsOrderLine
-    {
-        public int OrderLineId { get; set; }
-        public int OrderId { get; set; }
-        public int BookId { get; set; }
-    }
-}
